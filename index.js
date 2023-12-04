7bf948fb2d845363f86def2f4d1af1bb62168ad0
allmichel <allan.michel.info18@gmail.com>
2023-12-04 15:26:26 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -1,80 +1,56 @@
 const { readFileSync } = require("fs");
 
 
-  function formatarMoeda(valor) {
-    return new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-      minimumFractionDigits: 2,
-    }).format(valor / 100);
-  }
+function gerarFaturaHTML(fatura, pecas) {
+  let html = `
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Fatura - ${fatura.cliente}</title>
+      <style>
+        /* Adicione estilos CSS conforme necessário */
+      </style>
+    </head>
+    <body>
+      <h1>Fatura ${fatura.cliente}</h1>
+      <ul>
+  `;
 
-  function getPeca(pecas, apre) {
-    return pecas[apre.id];
-  }
-
-  function calcularCredito(pecas, apre) {
-    let creditos = 0;
-    creditos += Math.max(apre.audiencia - 30, 0);
-    if (getPeca(pecas, apre).tipo === "comedia")
-      creditos += Math.floor(apre.audiencia / 5);
-    return creditos;
-  }
-
-  function calcularTotalCreditos(pecas, apresentacoes) {
-    let totalCreditos = 0;
-    for (let apre of apresentacoes) {
-      totalCreditos += calcularCredito(pecas, apre);
-    }
-    return totalCreditos;
-  }
-
-  function calcularTotalApresentacao(pecas, apre) {
-    let total = 0;
-
-    switch (getPeca(pecas, apre).tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-        throw new Error(`Peça desconhecia: ${getPeca(pecas, apre).tipo}`);
-    }
-    return total;
-  }
-
-  function calcularTotalFatura(pecas, apresentacoes) {
-    let totalFatura = 0;
-    for (let apre of apresentacoes) {
-      const total = calcularTotalApresentacao(pecas, apre);
-      totalFatura += total;
-    }
-    return totalFatura;
-  }
-
-
-
-  function gerarFaturaStr(fatura, pecas) {
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
   for (let apre of fatura.apresentacoes) {
-    faturaStr += `  ${getPeca(pecas, apre).nome}: ${formatarMoeda(calcularTotalApresentacao(pecas, apre))} (${apre.audiencia} assentos)\n`;
-    }
+    const peca = getPeca(pecas, apre);
+    const totalApresentacao = calcularTotalApresentacao(pecas, apre);
+    html += `
+        <li>
+          <strong>${peca.nome}:</strong>
+          ${formatarMoeda(totalApresentacao)} (${apre.audiencia} assentos)
+        </li>
+    `;
   }
-  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(pecas, faturas.apresentacoes))}\n`;
-    faturaStr += `Créditos acumulados: ${calcularTotalCreditos(pecas, faturas.apresentacoes)} \n`;
-    return faturaStr;
+
+  const totalFatura = calcularTotalFatura(pecas, fatura.apresentacoes);
+  const totalCreditos = calcularTotalCreditos(pecas, fatura.apresentacoes);
+
+  html += `
+      </ul>
+      <p><strong>Valor total:</strong> ${formatarMoeda(totalFatura)}</p>
+      <p><strong>Créditos acumulados:</strong> ${totalCreditos}</p>
+    </body>
+    </html>
+  `;
+
+  return html;
+}
 
 
 const faturas = JSON.parse(readFileSync("./faturas.json"));
 const pecas = JSON.parse(readFileSync("./pecas.json"));
 const faturaStr = gerarFaturaStr(faturas, pecas);
+
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
 console.log(faturaStr);
+
+// Exemplo de como salvar o HTML em um arquivo (Node.js)
+const { writeFileSync } = require('fs');
+writeFileSync('./fatura.html', faturaHTML);
